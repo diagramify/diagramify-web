e4e8961bb097f1b21dc6fca21c39365eb5bd5dc1
sameer bhardwaj <bhardwajsameer7@gmail.com>
2025-03-19 15:45:43 +0000

    Initial Springboot project setup

---
+++ src/main/java/com/diagramify/DiagramifyWebApplication.java
@@ -0,0 +1,13 @@
+package com.diagramify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagramifyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagramifyWebApplication.class, args);
+	}
+
+}
